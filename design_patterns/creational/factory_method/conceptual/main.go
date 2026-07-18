fa86b86a4630df03ff5950f4221b30d3348ffc45
Nick <elanir358@gmail.com>
2026-07-18 17:14:50 +0300

    vscode settings & factory method creational pattern

---
+++ design_patterns/creational/factory_method/conceptual/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
+
+// go run .
+// Gun: AK47 gun
+// Power: 4
+// Gun: Musket gun
+// Power: 1
